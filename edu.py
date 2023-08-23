ab9e9402f9b1d82af491eb1014215477f7b080da
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-23 16:11:08 +0300

    spisky 2

--- edu.py
+++ edu.py
@@ -1,4 +1,12 @@
-a = int(input())
-if (a == 4):
-    raise(SystemExit(1))
-print("it's not foor lads chher up")
+import sys
+# if (sys.argv != 2):
+#     print("some shit happen")
+#     raise SystemExit(1)
+f = open("omega.txt")
+lines = f.readlines()#читает все строки из файла  в список
+f.close()
+
+fvalues = [float(line) for line in lines] #каждый элемент lines превращаем в число с плавающей точкой
+
+print("minimum: ", min(fvalues))
+print("maximum: ", max(fvalues))
