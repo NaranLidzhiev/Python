36ebf3e93e8ea5267a05c925e6969246aa035d2f
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-23 16:54:06 +0300

    mnojestva

--- edu.py
+++ edu.py
@@ -1,10 +1,9 @@
-f = open("portfolio.csv")
-portfolio = []
-for line in f:
-    fields = line.split(",")
-    name = fields[0]
-    shares = int(fields[1])
-    price = float(fields[2])
-    stock = (name, shares, price)
-    portfolio.append(stock)
-print(portfolio)
+numbers = set([1,2,3,4,5,6,7,8])#элемента множества не могут вызываться по индексу
+letters = set("HELLO")
+print(numbers)
+print(letters)#вывод h e l o, соответственно они не повторяют переменные и каждая переменная в множестве является уникальной
+numbers_and_letters = numbers | letters #объединения
+numbers_with_letters = numbers & letters # пересечение
+nimbers_minus_letters = numbers - letters #minus
+numbbers_alta_letters = numbers ^ letters #симметричная разность
+print(numbers_and_letters)
