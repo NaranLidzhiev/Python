75306ee8d1d07bd5aa9984ddadf4d6b8c667e41b
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-23 16:21:35 +0300

    cortegi

--- edu.py
+++ edu.py
@@ -1,12 +1,8 @@
-import sys
-# if (sys.argv != 2):
-#     print("some shit happen")
-#     raise SystemExit(1)
-f = open("omega.txt")
-lines = f.readlines()#читает все строки из файла  в список
-f.close()
-
-fvalues = [float(line) for line in lines] #каждый элемент lines превращаем в число с плавающей точкой
-
-print("minimum: ", min(fvalues))
-print("maximum: ", max(fvalues))
+stock = ("GOOG", 410, 120.22)# кортежи это вещь которое содрежит в себе несколько элементов, которые могут иметь абсолютно разную типизацию
+adress = ("www.python.org", 80)
+first_name = "ALEX"
+last_name = "Johanson"
+phone = "89035400437"
+person = (first_name, last_name, phone)
+name, number, price = stock
+print("name : ", name, "number : ", number, "price: ", price)
