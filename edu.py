f2bb176f399cadbc2d9ce45c83cb26bc5504cae1
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-25 19:56:18 +0300

    cycles

--- edu.py
+++ edu.py
@@ -1,9 +1,18 @@
-numbers = set([1,2,3,4,5,6,7,8])#элемента множества не могут вызываться по индексу
-letters = set("HELLO")
-print(numbers)
-print(letters)#вывод h e l o, соответственно они не повторяют переменные и каждая переменная в множестве является уникальной
-numbers_and_letters = numbers | letters #объединения
-numbers_with_letters = numbers & letters # пересечение
-nimbers_minus_letters = numbers - letters #minus
-numbbers_alta_letters = numbers ^ letters #симметричная разность
-print(numbers_and_letters)
+
+for n in [1,2,3,5,6,12]:
+    print(n)
+
+for n in range(1, 10):
+    print(n, n**2)
+
+for name in ["Alex", "Michael", "Jackson", "Stepa"]:
+    print(name)
+
+c = {
+    "GOOG": 213,
+    "CDM": 123,
+    "KSD": 112
+}
+
+for key in c:
+    print(key, c[key])
