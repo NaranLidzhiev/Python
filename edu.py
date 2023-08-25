f97d8efc81c97fa017fd75326b156748bc36520d
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-25 20:02:32 +0300

    functions

--- edu.py
+++ edu.py
@@ -1,18 +1,9 @@
 
-for n in [1,2,3,5,6,12]:
-    print(n)
+def cycle(a, b):
+        print(min(b))
+        print(max(a))
 
-for n in range(1, 10):
-    print(n, n**2)
+a = [1,2,3,4,5,2]
+b = [213,213,12,1,212,12,1]
 
-for name in ["Alex", "Michael", "Jackson", "Stepa"]:
-    print(name)
-
-c = {
-    "GOOG": 213,
-    "CDM": 123,
-    "KSD": 112
-}
-
-for key in c:
-    print(key, c[key])
+cycle(a,b)
