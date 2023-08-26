b665c8fb4d8dac0e090ccaa42725e2db7a7d4dfc
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-08-26 22:38:23 +0300

    exceptions

--- edu.py
+++ edu.py
@@ -1,21 +1,4 @@
-class Stack(object):
-    def __init__(self):
-        self.stack = []
-    def push(self, object):
-        self.stack.append(object)
-    def pop(self):
-        return self.stack.pop()
-    def length(self):
-        return len(self.stack)
-
-
-s = Stack()
-s.push("Dave")
-s.push([23,13])
-s.push([42, 45])
-p = s.pop()
-z = s.pop()
-print(p)
-print(z)
-print(s.stack)
-print(s.length)
+try:
+    f = open("file.txt")
+except IOError as e:
+    print(e)
