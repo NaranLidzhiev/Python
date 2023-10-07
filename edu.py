ac398b235b1238bf039b3d1c715ee73cea255750
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-10-08 02:08:50 +0300

    svyazniy spisok no na pythone(rabotaet parshivo)

--- edu.py
+++ edu.py
@@ -1,17 +1,55 @@
-def find_smallest(arr):
-    smallest_el = arr[0]
-    smallest_index = 0;
-    for i in range(1, len(arr)):
-        if (arr[i] < smallest_el):
-            smallest_el = arr[i]
-            smallest_index = i
-    return smallest_index
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
-def sort(arr):
-    new_arr = []
-    for i in range(len(arr)):
-        smallest = find_smallest(arr)
-        new_arr.append(arr.pop(smallest))
-    return new_arr
-arr = [13242,23,2,4,23,23,52,5]
-print(sort(arr))
+class LinkedList:
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def is_empty(self):
+        return self.first is None
+
+    def push_back(self, val):
+        p = Node(val)
+        if self.is_empty():
+            self.first = p
+            self.last = p
+            return
+        self.last.next = p
+        self.last = p
+
+    def print_list(self):
+        if self.is_empty():
+            return
+        p = self.first
+        while p:
+            print(p.val, end=" ")
+            p = p.next
+        print()
+
+    def reverse_list(self):
+        if self.is_empty() or self.first.next is None:
+            return
+
+        prev = None
+        current = self.first
+        next_node = None
+
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        self.first.next = prev
+
+if __name__ == "__main__":
+    l = LinkedList()
+    for i in range(1, 6):
+        l.push_back(str(i))
+
+    l.print_list()
+    l.reverse_list()
+    l.print_list()
