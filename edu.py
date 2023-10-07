fb23e1ad67dc0ec8f1598ae8ba6d8f10976df428
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-10-08 01:47:15 +0300

    smalles el

--- edu.py
+++ edu.py
@@ -1,16 +1,11 @@
-def binary_search(list, item):
-    low = 0
-    high = len(list)-1
-    while low <= high:
-        mid = int((low+high)/2)
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item: #если элемент посередине больше чем искомый, то верхней границей становится элемент, который стоит на позицию ниже срединного элемента
-            high = mid -1
-        else:
-            low = mid +1  #если элемент посередине меньше чем искомый, то нижней границей становится элемент, который стоит на позицию выше срединного элемента
-    return None
-#то есть мы смотрим больше или меньше искомого значения наш элемент который у нас в guess и основываясь на этом двигаем границы, если больше то все цифры больше guess отбрасываются, если меньше, то все цифры выше guess отбрасываются
-my_list = [1,2,4,6,7,8]
-print(binary_search(my_list, 7))
+def find_smallest(arr):
+    smallest_el = arr[0]
+    smallest_index = 0;
+    for i in range(1, len(arr)):
+        if (arr[i] < smallest_el):
+            smallest_el = arr[i]
+            smallest_index = i
+    return smallest_index
+
+arr = [13242,23,2,4,23,23,52,5]
+print(find_smallest(arr))
