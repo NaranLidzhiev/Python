907c160cc0a622294ca5e0f32b02885a0f7aa7b3
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-10-08 02:46:32 +0300

    dz1

--- edu.py
+++ edu.py
@@ -1,7 +1,25 @@
-def factorial_recursive(n):
-    if n == 1:
-        return n
-    else:
-        return n*factorial_recursive(n-1)
+n = int(input("n = "))
+m = int(input("m = "))
     
-print(factorial_recursive(5))
+if m > n:
+     raise ValueError("n должен быть больше m")
+    
+a = []
+sum_list = []
+
+for i in range(n):
+    a.append(int(input(f"a[{i}] = ")))
+
+i = 0
+while i < n - m + 1:
+    sum_val = 0
+    for j in range(i, i + m):
+        sum_val += a[j]
+    sum_list.append(sum_val)
+    i += 1
+
+for val in sum_list:
+    print(val)
+
+
+
