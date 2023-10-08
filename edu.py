99aed5b9f3a43e384b178a2279d8b7b57322c312
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-10-08 22:01:40 +0300

    sort viborom

--- edu.py
+++ edu.py
@@ -1,8 +1,18 @@
-def quick_sort(array):
-    if len(array)<2:
-        return array
-    pivot = array[0]
-    less = [i for i in array[1:] if i <= pivot]
-    greater = [i for i in array[1:] if i > pivot]
-    return quick_sort(less) +[pivot] +quick_sort(greater)
-print(quick_sort([1,1,3,3,2,34,24,43,2,4,234,23,43,4,5,5,43,5,25,2]))
+def find_min(array):
+    min_el = array[0]
+    min_index = 0
+    for i in range(1, len(array)):
+        if array[0] < min_el:
+            min_el = array[0]
+            min_index = i
+    return min_index
+
+def sort(array):
+    new_array = []
+    for i in range(len(array)):
+        smallest = find_min(array) #находим индекс минимального элемента
+        new_array.append(array.pop(smallest))
+        print(array) #одновременно забиваем в новыый массив минимальный элемент и одновременно удаляем его из первичного массива
+    return new_array
+
+sort([1,2,23,2,4,4,2,4,7,132,3,1])
