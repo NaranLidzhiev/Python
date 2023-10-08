1b2cd8d4118a5addadbf60a545f967e907e4c0dc
LidzhievNaran <lidzhievnaran483@gmail.com>
2023-10-08 22:33:27 +0300

    hash table

--- edu.py
+++ edu.py
@@ -1,18 +1,7 @@
-def find_min(array):
-    min_el = array[0]
-    min_index = 0
-    for i in range(1, len(array)):
-        if array[0] < min_el:
-            min_el = array[0]
-            min_index = i
-    return min_index
+book = dict()
 
-def sort(array):
-    new_array = []
-    for i in range(len(array)):
-        smallest = find_min(array) #находим индекс минимального элемента
-        new_array.append(array.pop(smallest))
-        print(array) #одновременно забиваем в новыый массив минимальный элемент и одновременно удаляем его из первичного массива
-    return new_array
-
-sort([1,2,23,2,4,4,2,4,7,132,3,1])
+book["apple"] = 0.67
+book["milk"] = 1.49
+book["avokado"] = 1.49
+print(book)
+print(book["avokado"])
